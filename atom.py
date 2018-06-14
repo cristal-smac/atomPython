73b940551c2e77e9fff9fba639d3e0c8e948964d
Rémi M <m.remimorvan@gmail.com>
2018-06-14 13:03:03 +0200

    Ajout trace

--- atom.py
+++ atom.py
@@ -13,42 +13,59 @@
    module atom
 """
 
+#
+# TODO :
+#
+# - Vérifier s'il y a des matchs à chaque rajout d'ordre, pas une fois que tous les ordres ont été rajoutés ?
+# - Corollaire : Modifier le prix du match comme étant le prix le plus ancien
+# - Regarder s'il existe un équivalent des TreeSet (Java) en Python pour les Orderbooks.asks/bids : on n'a besoin que des meilleurs éléments de la liste, pas d'avoir une liste entièrement triée
+
 import random
+import pylab as plt
+import numpy as np
+import pandas as pd
+import matplotlib.mlab as mlab
+
 
 class LimitOrder:
-    def __init__(self, asset, source, direction, price, qty, time_to_live=None):
+    def __init__(self, asset, source, direction, price, qty):
         self.asset = asset
         self.source = source
         self.price = price
         self.direction = direction.upper()
         self.qty = qty
-        self.time_to_live = time_to_live
     def __str__(self):
         return "%s %s at %.2f x %i from %s" % (self.direction, self.asset, self.price, self.qty, self.source.trader_id)
     def decrease_qty(self, q):
         self.qty -= q
-    def decrease_life(self):
-        if self.time_to_live != None:
-            self.time_to_live -= 1
-    def is_dead(self):
-        return self.time_to_live == 0
+    def attribute_list(self):
+        return [self.asset, self.source.__str__(), self.direction, self.price, self.qty]
 
-    
+
 class Trader(object):
     trader_count = 0
-    def __init__(self, available_assets=[]):
+    def __init__(self, available_assets=[], money=0, auto=False):
         Trader.trader_count += 1
         self.trader_id = Trader.trader_count
-        self.assets = dict()
+        self.money = money
         self.available_assets = available_assets
+        self.assets = dict()
+        for a in available_assets:
+            self.assets[a] = 0
     def __str__(self):
         return str(self.trader_id)
     def make_available(self, asset):
         available_assets.append(asset)
+        self.assets[asset] = 0
     def remove(self, asset):
         available_assets.remove(asset)
+        self.assets.pop(asset)
     def place_order(self, market):
         raise NotImplementedException
+    def add_money(self, n):
+        self.money += n
+    def add_assets(self, asset, n):
+        self.assets[asset] += n
     
 
 class ZITTrader(Trader):
@@ -56,7 +73,7 @@ class ZITTrader(Trader):
         return "ZIT " + super().__str__()
     def place_order(self, market):
         asset = random.choice(self.available_assets)
-        return LimitOrder(asset, self, random.choice(['ASK', 'BID']), random.randint(1, 100), random.randint(1, 100), random.randint(1, 5))
+        return LimitOrder(asset, self, random.choice(['ASK', 'BID']), random.randint(1, 100), random.randint(1, 100))
 
 
 class OrderBook:
@@ -66,26 +83,20 @@ class OrderBook:
         self.asks = []
         self.last_transaction = None
     def __str__(self):
-        return self.name + "\n" + str([str(x) for x in self.bids]) + "\n" + str([str(x) for x in self.bids])
-    def update_timer(self):
-        for o in self.asks:
-            o.decrease_life()
-        self.asks = [o for o in self.asks if not o.is_dead()]
-        for o in self.bids:
-            o.decrease_life()
-        self.bids = [o for o in self.bids if not o.is_dead()]        
-    def add_order(self, order):
+        return self.name + "\n" + str([str(x) for x in self.asks]) + "\n" + str([str(x) for x in self.bids])
+    def add_order(self, order, market):
         if order.direction == "BID":
             self.add_bid(order)
         else:
             self.add_ask(order)
+        print("Order;%s;%s;%s;%i;%i" % tuple(order.attribute_list()))
     def add_bid(self, order):
         self.bids.append(order)
         self.bids.sort(key=lambda o: -o.price)
     def add_ask(self, order):
         self.asks.append(order)
         self.asks.sort(key=lambda o: o.price)
-    def match(self):
+    def match(self, market): # Si une transaction est possible, l'effectue. Sinon, retourne None.
         if (len(self.asks) == 0) | (len(self.bids) == 0):
             return None
         if self.asks[0].price > self.bids[0].price:
@@ -93,28 +104,40 @@ class OrderBook:
         ask = self.asks.pop(0)
         bid = self.bids.pop(0)
         qty = min(ask.qty, bid.qty)
-        price = bid.price
+        price = bid.price # TODO : à changer
         if ask.qty > qty:
             ask.decrease_qty(qty)
             self.asks.insert(0, ask)
         if bid.qty > qty:
             bid.decrease_qty(qty)
             self.bids.insert(0, bid)
+        # On modifie les agents
+        ask.source.add_money(price*qty)
+        ask.source.add_assets(self.name, -qty)
+        bid.source.add_money(-price*qty)
+        bid.source.add_assets(self.name, qty)
         self.last_transaction = (bid.source, ask.source, price, qty)
+        market.prices[self.name] = price
+        # On affiche le prix
+        print("Price;%s;%s;%s;%i;%i" % (self.name, bid.source.__str__(), ask.source.__str__(), price, qty))
+        # On affiche les agents qui ont été modifiés
+        print("Agent;%s;%i;%s;%i" % (ask.source.__str__(), ask.source.money, self.name, ask.source.assets[self.name]))
+        if ask.source != bid.source: # Pour ne pas afficher deux fois la même ligne si l'agent ayant émis le ask et celui ayant émis le bid est le même.
+            print("Agent;%s;%i;%s;%i" % (bid.source.__str__(), bid.source.money, self.name, bid.source.assets[self.name]))
         return self.last_transaction
 
 
 class Market:
     def __init__(self):
+        self.time = 0
         self.traders = []
         self.orderbooks = dict()
-        self.time = 0
         self.prices = dict()
     def __str__(self):
         return "Market with %i traders on assets: %s" % (len(self.traders), str(self.orderbooks.keys()))
     def add_asset(self, orderbook):
         self.orderbooks[orderbook.name] = orderbook
-        self.prices[orderbook.name] = []
+        self.prices[orderbook.name] = None
     def remove_asset(self, orderbook):
         self.orderbooks.pop(orderbook.name, None)
         self.prices.pop(orderbook.name, None)
@@ -123,22 +146,39 @@ class Market:
             self.traders.append(trader)
     def remove_trader(self, trader):
         self.traders.remove(trader)
-    def run_once(self):
+    def update_time(self):
         self.time += 1
-        prices = []
+        at_least_one_price = False
+        for asset in self.orderbooks.keys():
+            if self.prices[asset] != None:
+                at_least_one_price = True
+                print("Tick;%i;%s;%i" % (self.time, asset, self.prices[asset]))
+        if not(at_least_one_price):
+            print("Tick;%i" % self.time)
+    def run_once(self):
+        self.update_time()
         random.shuffle(self.traders)
+        # Modifier la suite ?
+        # Ne devrait-on pas vérifier s'il y a des match après chaque ajout d'ordre ?
         for t in self.traders:
             decision = t.place_order(self)
             if decision != None:
                 if decision.asset in self.orderbooks:
-                    self.orderbooks[decision.asset].add_order(decision)
-        for o in self.orderbooks.values():
-            o.last_transaction = None
-            while o.match() != None:
+                    self.orderbooks[decision.asset].add_order(decision, self)
+        for ob in self.orderbooks.values():
+            ob.last_transaction = None
+            while ob.match(self) != None:
                 pass
-            o.update_timer()
-            if o.last_transaction != None:
-                self.prices[o.name].append(o.last_transaction[2])        
-                prices.append((o.name, self.time, o.last_transaction[2]))
-        return prices
-
+    def replay(self, order_list):
+        '''Run a list of orders of the form (asset, direction, price, qty).'''
+        t = Trader(self.orderbooks.keys())
+        m.add_trader(t)
+        for o in order_list:
+            self.time += 1
+            asset, direction, price, qty = o
+            order = LimitOrder(asset, t, direction, price, qty)
+            self.orderbooks[asset].add_order(order, self)
+            for ob in self.orderbooks.values():
+                ob.last_transaction = None
+                while ob.match(self) != None:
+                    pass
--- testatom.py
+++ testatom.py
@@ -1,41 +1,97 @@
-#-----------------------------------------
-#       ATOM Minimal Market Model
-#
-# Auteur  : Philippe MATHIEU
-# Labo    : CRISTAL, Equipe SMAC
-# Date    : 16/09/2010
-# contact : philippe.mathieu@univ-lille.fr
-#-----------------------------------------
-
 #!/usr/bin/python
 
 from atom import *
 
 m = Market()
-m.add_asset(OrderBook("Apple"))
-m.add_asset(OrderBook("Microsoft"))
+m.add_asset(OrderBook('Apple'))
+m.add_asset(OrderBook('Microsoft'))
 
-
-outfilename = 'prices.dat'
-outfile = open(outfilename, 'w')
-outfile.write("%s\t%s\t%s\n" % ("asset", "time", "price"))
-z = []
-for i in range(100):
-    t = ZITTrader(['Apple', 'Microsoft'])
+for i in range(2):
+    t = ZITTrader(['Apple'])
     m.add_trader(t)
-for i in range(1000):
-    p = m.run_once()
-    if p != []:
-        for x in p:
-            outfile.write("%s\t%i\t%.2f\n" % x)
-outfile.close()
+for i in range(5):
+    m.run_once()
 
-'''
-import sys
-import commands
-cmd = 'R --no-save <prices.R'
-failure, output = commands.getstatusoutput(cmd)
-if failure:
-    print("Failed")
-    sys.exit(1)
-'''
+## Test replay
+
+# m = Market(True, True, False)
+# m.add_asset(OrderBook('Apple'))
+
+# m.replay([('Apple', 'ASK', 50, 10, None), ('Apple', 'ASK', 40, 10, None), ('Apple', 'BID', 55, 30, None), ('Apple', 'ASK', 25, 15, None), ('Apple', 'BID', 30, 5, None)])
+
+# o, p, t = m.get_data()
+
+# ## Analyse données
+
+# def draw_prices_v0(p_df):
+#     # p_df columns: 'Time', 'Asset', 'Bider', 'Asker', 'Price', 'Qty'
+#     fig, ax = plt.subplots()
+#     for asset, df in p_df.groupby('Asset'):
+#         df.plot(x='Time', y='Price', ax=ax, label=asset)
+#         plt.ylabel('Price')
+#     plt.show()
+
+# def compute_prices(p_df):
+#     # p_df columns: 'Time', 'Asset', 'Bider', 'Asker', 'Price', 'Qty'
+#     prices = dict()
+#     p_df = p_df.loc[:,['Time','Asset','Price']]
+#     tmax = max(p_df.loc[:,'Time'])
+#     T = range(1,tmax+1)
+#     for asset, df in p_df.groupby('Asset'):
+#         df = df.loc[:,['Time','Price']].set_index('Time')
+#         P = []
+#         last_p = None
+#         for t in T:
+#             try:
+#                 last_p = df.loc[t]
+#             except:
+#                 pass
+#             P.append(last_p)
+#         prices[asset] = (T, P)
+#     return prices
+
+# def compute_returns(p_df):
+#     prices = compute_prices(p_df)
+#     returns = dict()
+#     for asset in prices.keys():
+#         (T,P) = prices[asset]
+#         P = np.array(P)
+#         R = (P[1:]-P[:-1])/P[:-1] # Returns
+#         mu = np.mean(R)
+#         sigma = np.sqrt(np.mean((R - mu)**2))
+#         R = (R - mu)/sigma # Centred reduced returns
+#         returns[asset] = (T[1:], R)
+#     return returns
+
+# def draw_prices(p_df):
+#     prices = compute_prices(p_df)
+#     for asset in prices.keys():
+#         (T,P) = prices[asset]
+#         plt.plot(T, P, '-', label=asset)
+#     plt.xlabel('Time')
+#     plt.ylabel('Price')
+#     plt.legend(loc='best')
+#     plt.show()
+
+# def draw_returns_hist(p_df, nb_pts=50):
+#     returns = compute_returns(p_df)
+#     hist = dict()
+#     for asset in returns.keys():
+#         (T, R) = returns[asset]
+#         Y, X, pat = plt.hist(R, nb_pts)
+#         Y = np.array(Y)
+#         R = (X[1:]+X[:-1])/2
+#         hist[asset] = (R, Y*R.size/(max(R)-min(R))/np.sum(Y))
+#     plt.clf()
+#     for asset in hist.keys():
+#         (R,D) = hist[asset]
+#         plt.semilogy(R, D, 'o')
+#     Y = mlab.normpdf(R, 0, 1)
+#     plt.semilogy(R, Y, '-k')
+#     plt.xlabel('Centred reduced returns')
+#     plt.ylabel('Density')
+#     plt.show()
+    
+#draw_prices(p)
+# draw_returns_hist(p, nb_pts=100)
+#draw_prices_v0(p)
