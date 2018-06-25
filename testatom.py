61bbcaffcd1b19cbe7563542d4f0df0ddf768d18
Rémi M <m.remimorvan@gmail.com>
2018-06-25 15:38:21 +0200

    Ajout de l'histogramme des rentabilités pour BTCUSD dans le notebook Atom

--- testatom.py
+++ testatom.py
@@ -11,7 +11,17 @@ from atom import *
 # m.print_state()
 # out.close()
 
-m = Market()
-m.add_asset(OrderBook('LVMH'))
-m.generate(['LVMH'], 2, 5)
-m.print_state()
+# m = Market()
+# m.add_asset(OrderBook('LVMH'))
+# m.generate(['LVMH'], 2, 5)
+# m.print_state()
+
+out = open('BTCUSD.dat', 'w')
+with open('Coinbase_BTCUSD_1h.csv', 'r') as file:
+	i = 0
+	for line in file:
+		i += 1
+		if i > 2:
+			l = line.split(',')
+			out.write("Price;BTCUSD;x;y;%i;qty;timestamp\n" % int(100*float(l[2])))
+out.close()
