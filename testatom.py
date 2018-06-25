1f49c3c7d0eeb0d513b217ae9ec9f634805f693c
Rémi M <m.remimorvan@gmail.com>
2018-06-25 19:15:00 +0200

    Premières simplifications d'Atom

--- testatom.py
+++ testatom.py
@@ -11,17 +11,6 @@ from atom import *
 # m.print_state()
 # out.close()
 
-# m = Market()
-# m.add_asset(OrderBook('LVMH'))
-# m.generate(['LVMH'], 2, 5)
-# m.print_state()
-
-out = open('BTCUSD.dat', 'w')
-with open('Coinbase_BTCUSD_1h.csv', 'r') as file:
-	i = 0
-	for line in file:
-		i += 1
-		if i > 2:
-			l = line.split(',')
-			out.write("Price;BTCUSD;x;y;%i;qty;timestamp\n" % int(100*float(l[2])))
-out.close()
+m = Market(['LVMH', 'Apple'], print_orderbooks=True)
+m.generate(2, 5)
+m.print_state()
